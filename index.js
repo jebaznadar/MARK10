81f6d8492eaf246a407d07e5ee2082b7d8f86649
jebaznadar <126007453+jebaznadar@users.noreply.github.com>
2023-05-14 19:06:36 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,4 @@
+const billAmount =  document.querySelector("#bill-amount"); 
+const checkButton  = document.querySelector("check-button");
+const cashGiven = document.querySelector("cash-given");
+checkButton.addEventListener("click" ()=>console.log(cashGiven.value);) 
