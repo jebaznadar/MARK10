6545f00b2c9af402a9cd77790d60f8742452a84f
jebaznadar <126007453+jebaznadar@users.noreply.github.com>
2023-05-24 15:53:55 +0530

    without bonus

--- index.js
+++ index.js
@@ -5,48 +5,48 @@ const cashGiven = document.querySelector("#cash-given");
 const errorMessage = document.querySelector("#error-message")
 const noOfNotes = document.querySelectorAll(".no-of-notes");
 const availableNotes = [ 2000, 500, 100, 20, 10, 5, 1];
-const changeTable = document.querySelector(".change-table")
+const changeTable = document.querySelector("#change-table")
 const cashGivenInput = document.querySelector(".cash-input")
 
-nextButton.addEventListener("click", function validateBillAmount(){
-    hideError();
-    cashGivenInput.style.display = "none";
-    changeTable.style.display = "none ";
+// nextButton.addEventListener("click", function validateBillAmount(){
+//     hideError();
+//     cashGivenInput.style.display = "none";
+//     changeTable.style.display = "none ";
      
-    if (billAmount.value > 0){
-    nextButton.style.display = "none" ; 
-    cashGivenInput.style.display = "block"  ;
+//     if (billAmount.value > 0){
+//     nextButton.style.display = "none" ; 
+//     cashGivenInput.style.display = "block"  ;
     
-   }
-   else{
+//    }
+//    else{
     
-    showError("enter bill amount value greater than zero ")
+//     showError("enter bill amount value greater than zero ")
     
-   }
-} ) ;
+//    }
+// } ) ;
 
 checkButton.addEventListener("click", function validateBillAndCashAmount (){
    clearNoOfNotes();
-   hideError();
-    if (billAmount.value >0 && cashGiven > 0 ){
+    hideError();
+    if (billAmount.value >0 ){
         
         if(cashGiven.value >= billAmount.value){
             
           const amountToBeReturned = cashGiven.value - billAmount.value;
           calculatedChange(amountToBeReturned);
-          changeTable.style.display ="block";
+        //   changeTable.style.display ="block";
 
         }
         else{
             
-            showError( "do you want to wash plates?")
+            showMessage("Do you want to wash plates 🍽️?");
            
         }
 
     }
     else{
           
-        showError("Enter valid bill amount and cash given to continue ")
+        showMessage("Bill Amount should be greater than zero ");
         
     }
 });
@@ -64,14 +64,18 @@ function hideError(){
     errorMessage.style.display ="none" ;
     
 }
-function showError(text){
+function showMessage (text){
     errorMessage.style.display = "block";
-    errorMessage.innerText= text;
-    changeTable.style.display = "none";
-    cashGivenInput.style.display = "none"
+    errorMessage.innerText = text ;
 }
+// function showError(text){
+//     errorMessage.style.display = "block";
+//     errorMessage.innerText= text;
+//     changeTable.style.display = "none";
+//     cashGivenInput.style.display = "none"
+// }
 function clearNoOfNotes(){
     for(let i=0; i<availableNotes.length;i++){
-        noOfNotes[i].style.display = "" ;
+        noOfNotes[i].innerText = " " ;
     }
 }
